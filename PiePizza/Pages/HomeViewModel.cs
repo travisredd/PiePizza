e1cc1bcbb37fbd67db6086bbb3309e9fb66abd24
travisredd <travisredd@mail.weber.edu>
2019-05-27 16:17:41 -0600

    ....

---
+++ PiePizza/Pages/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PiePizza.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PiePizza.Pages
+{
+    public class HomeViewModel
+    {
+        public List<Pie> Pies { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ PiePizza/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PiePizza.Models;
+
+namespace PiePizza.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public Enum Pies { get; set; }
+
+        [BindProperty]
+        public Pie Pie { get; set; }
+
+        public IPieRepository pieRepository { get; set; }
+
+        public IndexModel(IPieRepository pieRepository)
+        {
+            this.pieRepository = pieRepository;
+        }
+
+        public Enum OnGetAsync()
+        {
+            var newPie = pieRepository.GetAllPies();
+
+            return Pies;
+        }
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            
+            
+            return RedirectToPage("/Index");
+        }
+    }
+}
